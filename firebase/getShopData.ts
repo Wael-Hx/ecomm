88b456f4eec30b4d0036b14102cb9a9879a2d34b
Wael-Hx <wael.lhb@gmail.com>
2021-08-28 17:40:26 +0100

    getShopData

---
+++ firebase/getShopData.ts
@@ -0,0 +1,26 @@
+import { Brand, Shop, Smartphone } from "../types";
+import { db } from "./config";
+
+export default async function getShopData(): Promise<Shop> {
+  try {
+    const brands: Brand[] = [],
+      smartphones: Smartphone[] = [];
+
+    const [brandsQuery, smartphonesQuery] = await Promise.all([
+      db.collection("brands").orderBy("name").get(),
+      db.collection("shop").orderBy("brand").get(),
+    ]);
+    brandsQuery.forEach((brand) => brands.push(brand.data() as Brand));
+    smartphonesQuery.forEach((smartphone) =>
+      smartphones.push(smartphone.data() as Smartphone)
+    );
+
+    return {
+      brands,
+      smartphones,
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
