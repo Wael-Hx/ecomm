13496ee3eb506602f266bac719ecbc1399e26f8e
Wael-Hx <wael.lhb@gmail.com>
2021-09-04 13:40:28 +0100

    featured card animations

--- components/fronpage/featured/FeaturedCard.tsx
+++ components/fronpage/featured/FeaturedCard.tsx
@@ -1,8 +1,17 @@
 import { useApolloClient } from "@apollo/client";
-import { Box, HStack, Image, StackProps, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  BoxProps,
+  HStack,
+  Image,
+  StackProps,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { motion, Variants } from "framer-motion";
 import { GET_SHOP } from "../../../graphql/queries";
 import { Shop, Smartphone } from "../../../types";
+import Specs from "./Specs";
 
 const getItem = (smartphones: Smartphone[]) => {
   const idx = Math.ceil(Math.random() * (smartphones.length - 1));
@@ -10,6 +19,8 @@ const getItem = (smartphones: Smartphone[]) => {
 };
 
 const AnimatedCard = motion<StackProps>(VStack);
+const MotionBox = motion<BoxProps>(Box);
+
 const card: Variants = {
   visible: {
     opacity: 1,
@@ -17,6 +28,32 @@ const card: Variants = {
     transition: { ease: "easeInOut", duration: 1 },
   },
   hidden: { opacity: 0, x: 30 },
+};
+
+const imageAnimation: Variants = {
+  hover: {
+    scale: 0.9,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+    },
+  },
+  idle: {
+    scale: 1,
+  },
+};
+
+const name: Variants = {
+  hover: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  idle: {
+    opacity: 0,
+  },
 };
 
 const FeaturedCard = () => {
@@ -33,6 +70,8 @@ const FeaturedCard = () => {
 
   return (
     <AnimatedCard
+      fontFamily="Montserrat"
+      fontSize=".87rem"
       alignItems="start"
       w={{ base: "80%", md: "35%" }}
       spacing="2"
@@ -43,7 +82,7 @@ const FeaturedCard = () => {
       animate="visible"
       h="65%"
     >
-      <HStack spacing="1">
+      <HStack fontWeight="light" spacing="1">
         <HStack w="5ch" h="3ch" bg="black">
           <Text as="h4" p="1" color="white">
             -20%
@@ -51,9 +90,38 @@ const FeaturedCard = () => {
         </HStack>
         <Text as="h4"> {((phone.price * 80) / 100).toFixed(2)}$ </Text>
       </HStack>
-      <Box maxW="67%" w="67%" css={{ aspectRatio: "9/12" }}>
+      <MotionBox
+        background="url(/loading.svg) center no-repeat"
+        backgroundSize="25% 25%"
+        maxW="67%"
+        w="67%"
+        css={{ aspectRatio: "9/12" }}
+        pos="relative"
+        initial="idle"
+        whileHover="hover"
+        whileFocus="hover"
+        whileTap="hover"
+        variants={imageAnimation}
+      >
         <Image src={phone.image} alt={phone.name} />
-      </Box>
+        <Specs
+          options={phone.specs}
+          ram={phone.ram}
+          storage={phone.storage}
+          screenSize={phone.size}
+        />
+        <MotionBox
+          pos="absolute"
+          top="-7%"
+          left="-5%"
+          variants={name}
+          as="h4"
+          fontWeight="medium"
+          textTransform="uppercase"
+        >
+          {phone.name}
+        </MotionBox>
+      </MotionBox>
     </AnimatedCard>
   );
 };
