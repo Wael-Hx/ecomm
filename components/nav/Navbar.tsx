057205d2de57c19018d69c72c5903fd5cc72cc5b
Wael-Hx <wael.lhb@gmail.com>
2021-09-08 18:49:43 +0100

    navbar home link + search box

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,9 +1,24 @@
-import { Flex, Heading, HStack, Icon } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  InputGroup,
+} from "@chakra-ui/react";
 import { FiSmartphone } from "react-icons/fi";
 import { BiSearchAlt } from "react-icons/bi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FormEvent } from "react";
+import NextLink from "next/link";
 
 const Navbar = () => {
+  const submitSearch = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <Flex
       position="sticky"
@@ -18,20 +33,37 @@ const Navbar = () => {
     >
       <HStack paddingInline={["1em", "3em"]} spacing="1">
         <Icon as={FiSmartphone} boxSize="4vmin" />
-        <Heading fontFamily="Raleway" fontSize="clamp(1.2rem ,2.6vmin,4rem )">
-          {" "}
-          SHOP{" "}
-        </Heading>
+        <NextLink href="/">
+          <a>
+            <Heading
+              fontFamily="Raleway"
+              fontSize="clamp(1.2rem ,2.6vmin,4rem )"
+            >
+              {" "}
+              SHOP{" "}
+            </Heading>
+          </a>
+        </NextLink>
       </HStack>
 
-      <HStack paddingInline={["1em", "3em"]} spacing="2">
-        <Icon as={BiSearchAlt} boxSize="4vmin" />
-        <HStack spacing="1">
-          <Icon as={AiOutlineShoppingCart} boxSize="4vmin" />
-          <Heading fontWeight="normal" fontSize="clamp(1.2rem ,2.6vmin,4rem )">
-            0
-          </Heading>
-        </HStack>
+      <Box as="form" w="75%" onSubmit={submitSearch}>
+        <InputGroup paddingInline={["1ch", "2ch"]}>
+          <Input type="text" placeholder="search..." />
+          <IconButton
+            marginInline="0.5rem"
+            variant="outline"
+            icon={<BiSearchAlt color="gray.300" />}
+            aria-label="search"
+            type="submit"
+          />
+        </InputGroup>
+      </Box>
+
+      <HStack paddingInline={["1ch", "2ch"]} spacing="1">
+        <Icon as={AiOutlineShoppingCart} boxSize="4vmin" />
+        <Heading fontWeight="normal" fontSize="clamp(1.2rem ,2.6vmin,4rem )">
+          0
+        </Heading>
       </HStack>
     </Flex>
   );
