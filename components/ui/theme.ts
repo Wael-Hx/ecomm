f92f3c6aeddfe0eaa8a8999186bd8ae8b72eb5a4
Wael-Hx <wael.lhb@gmail.com>
2021-09-09 16:55:08 +0100

    global typography

--- components/ui/theme.ts
+++ components/ui/theme.ts
@@ -7,6 +7,35 @@ const config: ThemeConfig = {
 
 const theme = extendTheme<Theme>({
   config,
+  styles: {
+    global: {
+      h2: {
+        fontFamily: "Raleway",
+        fontSize: "clamp(1.2rem ,2.6vmin,4rem )",
+        fontWeight: "bold",
+      },
+      h3: {
+        fontSize: "clamp(0.8rem, 2.5vmin, 4rem)",
+        fontWeight: "bold",
+        fontFamily: "Raleway",
+      },
+      h4: {
+        fontFamily: "Montserrat",
+        fontSize: "clamp(0.6rem , 0.9vmax , 1.2vmax)",
+        fontWeight: "normal",
+      },
+      h5: {
+        fontFamily: "Raleway",
+        fontSize: "clamp(0.6rem , 0.9vmax , 1.2vmax)",
+        fontWeight: "normal",
+      },
+      h6: {
+        fontFamily: "Montserrat",
+        fontSize: "clamp(0.6rem , 0.75vmax , 1.1vmax)",
+        fontWeight: "normal",
+      },
+    },
+  },
 });
 
 export default theme;
