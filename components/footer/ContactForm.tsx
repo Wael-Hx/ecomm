b59acce3a2efaf0aec6ea8dc6d1d7e6134d13114
esdevz <wael.lhb@gmail.com>
2022-01-10 19:33:51 +0100

    keep form data on error

--- components/footer/ContactForm.tsx
+++ components/footer/ContactForm.tsx
@@ -73,7 +73,7 @@ const ContactFrom = () => {
         loading: false,
       });
     } finally {
-      setFormData(initialFormData);
+      if (!status.error) setFormData(initialFormData);
     }
   };
 
